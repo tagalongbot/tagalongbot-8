2399ed315703b9ee1bc6b86b0e4264361d7b5b6a
Glitch (bevl-beauty) <none>
2018-04-07 18:07:16 +0000

    💀🚶 Checkpoint ./server.js:701400/12

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ let express = require('express');
 let app = express();
 
 let handleAI = require('./routes/ai');
-let exportData = require('./routes/export-data');
+// let exportData = require('./routes/export-data');
 
 app.get('/ai', handleAI);
-app.get('/export-data', exportData);
+// app.get('/export-data', exportData);
 
 app.listen(3000, () => console.log('Running on PORT 3000'));
