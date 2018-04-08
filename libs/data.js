1f9c4b0abab01dde05923110a59ae45af2870bfb
Glitch (bevl-beauty) <none>
2018-04-08 21:03:12 +0000

    🐫💋 Checkpoint ./libs/data.js:701400/571

--- libs/data.js
+++ libs/data.js
@@ -1,7 +1,23 @@
-let {  } = process.env;
-let request = require('request');
+let { DATABASE_API_TOKEN } = process.env;
+let fetch = require('node-fetch');
 
-let getActiveProviders = () => {}
+let createOptions = (endpoint) => {
+  let method = 'GET';
+  let url = `http://api.bevlbeauty.com/api/Edwin${endpoint}`;
+  let qs = { token: DATABASE_API_TOKEN };
+  let options = { method, url, qs };
+  return options;
+}
+
+let handleData = (error, response, body) => {
+  
+}
+
+let getActiveProviders = () => {
+  let options = createOptions('/GetProviders');
+  fetch(options, handleData);
+}
+
 let getServices = () => {}
 let getProviderServices = () => {}
 let getActivePromos = () => {}
