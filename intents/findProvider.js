f7108f98e16dca4ec79072d1f3a0913ede698677
Glitch (bevl-beauty) <none>
2018-04-08 21:44:30 +0000

    🐧🚧 Checkpoint ./intents/findProvider.js:701400/69

--- intents/findProvider.js
+++ intents/findProvider.js
@@ -1,12 +1,14 @@
 let { createTextMessage } = require('../libs/bots');
 let { getActiveProviders } = require('../libs/data');
 
+
+
 let filterProviders = (parameters, providers) => {
   let { provider, provider_type, Quality, Location, brandname} = parameters;
   let state_us = parameters['geo-state-us'];
   let city_us = parameters['geo-city'];
-  
-  let filteredProviders = providers.filter(() => {
+
+  let filteredProviders = providers.filter((providerObj) => {
     
   });
   
@@ -17,7 +19,7 @@ let findProvider = async ({ res, parameters }) => {
   let activeProviders = await getActiveProviders();
   let filteredProviders = filterProviders(parameters, activeProviders);
   res.send(filteredProviders);
-  
+
   // let textMsg = createTextMessage('Testing');
   // let messages = [textMsg];
   // res.send({ messages });
