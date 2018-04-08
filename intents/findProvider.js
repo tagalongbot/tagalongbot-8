316c4cf3e88b247bff613e6db925fbf72778daae
Glitch (bevl-beauty) <none>
2018-04-08 21:54:32 +0000

    🍢🧀 Checkpoint ./intents/findProvider.js:701400/360 ./routes/ai.js:701400/8

--- intents/findProvider.js
+++ intents/findProvider.js
@@ -1,16 +1,16 @@
 let { createTextMessage } = require('../libs/bots');
 let { getActiveProviders } = require('../libs/data');
 
-
+let byZipCode = (zip_code) => (provider) => {
+  console.log('Zip Code', zip_code);
+  return provider['postal'].includes(zip_code);
+}
 
 let filterProviders = (parameters, providers) => {
-  let { provider, provider_type, Quality, Location, brandname} = parameters;
-  let state_us = parameters['geo-state-us'];
-  let city_us = parameters['geo-city'];
+  let { provider, provider_type, state, city, zip_code, quality, location, brand_name} = parameters;
 
-  let filteredProviders = providers.filter((providerObj) => {
-    
-  });
+  let filteredProviders = providers
+    .filter(byZipCode(zip_code));
   
   return filteredProviders;
 }
