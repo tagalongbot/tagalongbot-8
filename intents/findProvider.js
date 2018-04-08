d88bcb9ebd464e531a32426bf37b9f88ea95e290
Glitch (bevl-beauty) <none>
2018-04-08 21:13:55 +0000

    🚛💧 Checkpoint ./libs/data.js:701400/1435 ./intents/findProvider.js:701400/387

--- intents/findProvider.js
+++ intents/findProvider.js
@@ -1,9 +1,19 @@
 let { createTextMessage } = require('../libs/bots');
+let { getActiveProviders } = require('../libs/data');
 
-let findProvider = ({ res, parameters }) => {
-  let textMsg = createTextMessage('Testing');
-  let messages = [textMsg];
-  res.send({ messages });
+let filterProviders
+
+
+let findProvider = async ({ res, parameters }) => {
+  let activeProviders = await getActiveProviders();
+  let filteredProviders = activeProviders.filter(byParameters);
+  
+  
+  res.send(activeProviders);
+  
+  // let textMsg = createTextMessage('Testing');
+  // let messages = [textMsg];
+  // res.send({ messages });
 }
 
 module.exports = findProvider;
--- libs/data.js
+++ libs/data.js
@@ -1,25 +1,30 @@
 let { DATABASE_API_TOKEN } = process.env;
 let fetch = require('node-fetch');
 
-let createOptions = (endpoint) => {
+let toJSON = (res) => res.json();
+
+let createFetchData = (endpoint) => {
   let method = 'GET';
-  let url = `http://api.bevlbeauty.com/api/Edwin${endpoint}`;
-  let qs = { token: DATABASE_API_TOKEN };
-  let options = { method, url, qs };
-  return options;
+  let url = `http://api.bevlbeauty.com/api/Edwin${endpoint}?token=${DATABASE_API_TOKEN}`;
+  let body = {};
+  let options = { method };
+  return { url, options };
 }
 
-let handleData = (error, response, body) => {
-  
+let createEndpoint = (endpoint) => async () => {
+  let { url, options } = createFetchData(endpoint);
+  let data = await fetch(url, options).then(toJSON);
+  return data;
 }
 
-let getActiveProviders = () => {
-  let options = createOptions('/GetProviders');
-  fetch(options, handleData);
-}
+let getActiveProviders = createEndpoint('/GetProviders');
+let getServices = createEndpoint('/GetServices');
+let getProviderServices = createEndpoint('/GetProviderServices');
+let getActivePromos = createEndpoint('/GetPromos');
 
-let getServices = () => {}
-let getProviderServices = () => {}
-let getActivePromos = () => {}
-
-module.exports = {};
+module.exports = {
+  getActiveProviders,
+  getServices,
+  getProviderServices,
+  getActivePromos,
+};
