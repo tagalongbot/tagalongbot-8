699957eed531aff5802a05371e5327d0f69346f8
Glitch (bevl-beauty) <none>
2018-04-08 22:56:35 +0000

    🌲🌨 Checkpoint ./routes/getProviderServices.js:701400/1163

--- routes/getProviderServices.js
+++ routes/getProviderServices.js
@@ -1,14 +1,27 @@
+let { createGallery } = require('../libs/bots'); 
 let { getServices, getProviderServices } = require('../libs/data');
+
+let toGalleryElement = (provider_name) => (service) => {
+  let title = service.service_name;
+  let subtitle = `Service provided by ${provider_name}`.slice(0, 80);
+  let image_url = service.service_photo_uri;
+  
+
+}
 
 let getProviderListOfServices = async ({ query }, res) => {
   let { provider_id } = query;
 
   let providerServices = await getProviderServices();
   let servicesFromProvider = providerServices
-    .filter((service) => service.providerid === provider_id)
+    .filter((service) => service.providerid === Number(provider_id))
     .map(service => service.serviceid);
   
+  let services = await getServices();
+  let matchedServices = services
+    .filter(service => servicesFromProvider.includes(service.serviceid));
   
+  res.send(matchedServices);
 }
 
 module.exports = getProviderListOfServices;
