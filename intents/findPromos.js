cc2456a831ebeb0fa8fdf938d4dfa97d6a15788c
Glitch (bevl-beauty) <none>
2018-04-09 19:16:19 +0000

    🎽💖 Checkpoint ./intents/findPromos.js:701400/1639

--- intents/findPromos.js
+++ intents/findPromos.js
@@ -2,45 +2,18 @@ let { BASEURL } = process.env;
 let { createTextMessage, createGallery } = require('../libs/bots');
 let { getServices, getActivePromos } = require('../libs/data');
 
-let byZipCode = (zip_code) => (provider) => {
-  if (!zip_code) return true;
-  return provider['postal'].includes(zip_code);
-}
-
-let byState = (state) => (provider) => {
-  if (!state) return true;
-  return provider['state'].toLowerCase().includes(state.toLowerCase());
-}
-
-let byCity = (city) => (provider) => {
-  if (!city) return true;
-  return provider['city'].toLowerCase().includes(city.toLowerCase());
-}
-
-let filterProviders = (parameters, providers) => {
-  let { provider, provider_type, state, city, zip_code, quality, location, brand_name} = parameters;
-
-  let filteredProviders = providers
-    .filter(byZipCode(zip_code))
-    .filter(byState(state))
-    .filter(byCity(city));
-    // .filter(byQuality(quality)
-  
-  return filteredProviders;
-}
-
-let toGalleryElement = (provider) => {
-  let title = provider.practice_name.slice(0, 80);
-  let subtitle = `${provider.first_name} ${provider.last_name} | ${provider.address}`;
-  let image_url = provider.practice_panel_photo_uri;
+let toGalleryElement = (promo) => {
+  let title = promo.promo_name.slice(0, 80);
+  let subtitle = promo.promo_terms;
+  let image_url = promo.promo_photo_uri;
 
   let btn1 = {
-    title: 'View Services',
+    title: 'View Promo Details',
     type: 'json_plugin_url',
-    url: `${BASEURL}/provider/services?provider_id=${provider.providerid}&provider_name=${encodeURIComponent(provider.practice_name)}`
+    url: `${BASEURL}/promo/details?promo_id=${promo.promoid}`
   }
 
- let btn2 = {
+  let btn2 = {
     title: 'View Promos',
     type: 'json_plugin_url',
     url: `${BASEURL}/provider/promos?provider_id=${provider.providerid}&provider_name=${encodeURIComponent(provider.practice_name)}`
@@ -58,8 +31,8 @@ let findPromos = async ({ res, parameters, user}) => {
   
   let services = await getServices();
   
-  let servicesAskedFor = services.filter((service) => {
-    let serviceName = service.service_name.toLowerCase();
+  let servicesAskedFor = services.filter(({ service_name }) => {
+    let serviceName = service_name.toLowerCase();
     let procedureName = procedure.toLowerCase();
     let brandName = procedure.toLowerCase();
     
@@ -69,18 +42,17 @@ let findPromos = async ({ res, parameters, user}) => {
   let servicesNumArr = servicesAskedFor.map((service) => service.serviceid);
   
   let activePromos = await getActivePromos();
+  let matchingPromos = activePromos.filter(({ serviceid }) => servicesNumArr.includes(serviceid));
   
-  let foundProviders = filterProviders(parameters, activePromos);
-
-  if (!foundProviders[0]) {
-    let redirect_to_blocks = ['No Providers Found'];
+  if (!matchingPromos[0]) {
+    let redirect_to_blocks = ['No Promos Found'];
     res.send({ redirect_to_blocks });
     return;
   }
   
   let txtMsg = createTextMessage(`${first_name} here's what I found`);
   
-  let galleryData = foundProviders.map(toGalleryElement).slice(0, 10);
+  let galleryData = matchingPromos.map(toGalleryElement).slice(0, 5);
   let gallery = createGallery(galleryData);
   
   let messages = [txtMsg, gallery];
