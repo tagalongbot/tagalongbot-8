8ed5490054c5c74c851afd336b546f2cdc667695
Glitch (bevl-beauty2) <none>
2018-04-20 06:45:02 +0000

    🌵🏓 Checkpoint ./routes/search/providers.js:701400/906

--- routes/search/providers.js
+++ routes/search/providers.js
@@ -1,1 +1,12 @@
-let 
+let { PRACTICE_DATABASE_BASE_ID, USERS_BASE_ID, SERVICES_BASE_ID } = process.env;
+let { createGallery } = require('../libs/bots');
+let { getTable, getAllDataFromTable, findTableData, createTableData, updateTableData, destroyTableData } = require('../libs/data');
+
+// Tables
+let getUsersTable = getTable('Users');
+let getUsersTable = getTable('Practices');
+// let getUsersTable = getTable('Services');
+
+let getUsers = getAllDataFromTable(USERS_BASE_ID);
+let getUsers = getAllDataFromTable(SERVICES_BASE_ID);
+let getUsers = getAllDataFromTable();
