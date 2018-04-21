49543f6a14f576334a09fc9dd030659c17701643
Glitch (bevl-beauty2) <none>
2018-04-21 22:26:08 +0000

    🐔🚪 Checkpoint ./routes/getPromoProviders.js:701400/4389

--- routes/getPromoProviders.js
+++ routes/getPromoProviders.js
@@ -1,56 +1,75 @@
 let { BASEURL } = process.env;
 let { createGallery } = require('../libs/bots');
-let { getActivePromos, getActiveProviders } = require('../libs/data');
+let { searchProviders } = require('../libs/providers');
+let { getTable, findTableData } = require('../libs/data');
 
-let toGalleryElement = (promo) => ({ id: provider_id, fields: provider }) => {
+let express = require('express');
+let router = express.Router();
+
+let toGalleryElement = ({ id: provider_id, fields: provider }) => {
   let title = provider['Practice Name'].slice(0, 80);
   let subtitle = `${provider['Main Provider']} | ${provider['Practice Address']}`;
   let image_url = provider['Main Provider Image'][0].url;
 
   let btn1 = {
-    title: 'Claim Promotion',
+    title: 'View Promos',
     type: 'json_plugin_url',
-    url: `${BASEURL}/promo/claim?provider_id=${provider_id}&promo_id=${promo.promoid}`
+    url: `${BASEURL}/provider/promos?provider_id=${provider_id}&provider_name=${encodeURIComponent(provider['Practice Name'])}`
   }
 
   let btn2 = {
-    title: 'View Provider Promos',
-    type: 'json_plugin_url',
-    url: `${BASEURL}/provider/promos?provider_id=${provider_id}&provider_name=${encodeURIComponent(provider.practice_name)}`
-  }
-
-  let btn3 = {
     title: 'View Services',
     type: 'json_plugin_url',
-    url: `${BASEURL}/provider/services?provider_id=${provider_id}&provider_name=${encodeURIComponent(provider.practice_name)}`
+    url: `${BASEURL}/provider/services?provider_id=${provider_id}&provider_name=${encodeURIComponent(provider['Practice Name'])}`
   }
-  
-  let buttons = [btn1, btn2, btn3];
-  
-  let element = { title, subtitle, image_url, buttons };
+
+  let buttons = [btn1, btn2];
+
+  let element = { title, subtitle, image_url, buttons};
   return element;
 }
 
-let getPromoProviders = async ({ query }, res) => {
-  let { promo_id } = query;
+let searchServiceProviders = async ({ query }, res) => {
+  let { service_id, service_name } = query;
+  let set_attributes = { service_id, service_name };
+  let redirect_to_blocks = ['Search Service Providers'];
+  res.send({ set_attributes, redirect_to_blocks });
+}
 
-  let activePromos = await getActivePromos();
-  let promo = activePromos.find((promo) => promo.promoid === Number(promo_id));
+let getServiceProviders = async ({ query, params }, res) => {
+  let { search_service_providers_state, search_service_providers_city, search_service_providers_zip_code } = query;
+  let { search_type } = params;
+  let { service_name } = query;
+  
+  let providers = await searchProviders({
+    search_providers_state: search_service_providers_state,
+    search_providers_city: search_service_providers_city,
+    search_providers_zip_code: search_service_providers_zip_code,
+  }, search_type);
 
-  if (!promo) {
-    let redirect_to_blocks = ['Provider No Longer Active'];
+  if (!providers[0]) {
+    let redirect_to_blocks = ['No Providers Found'];
     res.send({ redirect_to_blocks });
     return;
   }
 
-  let activeProviders = await getActiveProviders();
-  
-  let provider = activeProviders.find((provider) => provider.providerid === promo.providerid);
-  
-  let promosGalleryData = [provider].map(toGalleryElement(promo));
-  let servicesGallery = createGallery(promosGalleryData);
-  let messages = [servicesGallery];
+  let filteredProviders = providers.filter(({ fields: provider }) => {
+    return provider['Practice Services'].includes(service_name);
+  });
+
+  if (!filteredProviders[0]) {
+    let redirect_to_blocks = ['No Providers Found'];
+    res.send({ redirect_to_blocks });
+    return;
+  }
+
+  let providersGalleryData = filteredProviders.map(toGalleryElement);
+  let providersGallery = createGallery(providersGalleryData);
+  let messages = [providersGallery];
   res.send({ messages });
 }
 
-module.exports = getPromoProviders;
+router.get('/providers', searchServiceProviders);
+router.get('/providers/:search_type', getServiceProviders);
+
+module.exports = router;
