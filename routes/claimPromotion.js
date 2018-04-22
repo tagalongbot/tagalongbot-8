951465d450a1177332b8eb166670a6a435ff1b35
Glitch (bevl-beauty2) <none>
2018-04-22 13:33:47 +0000

    🌧🍍 Checkpoint ./routes/claimPromotion.js:701400/2448

--- routes/claimPromotion.js
+++ routes/claimPromotion.js
@@ -1,19 +1,51 @@
-let { BASEURL } = process.env;
+let { BASEURL, USERS_BASE_ID } = process.env;
 let { createButtonMessage } = require('../libs/bots');
 let { createURL } = require('../libs/helpers');
-let { getTable, findTableData, updateTableData } = require('../libs/data');
+let { getTable, getAllDataFromTable, findTableData, createTableData, updateTableData } = require('../libs/data');
 
+let getUsersTable = getTable('Users');
 let getPromosTable = getTable('Promos');
 
-let claimPromotion = ({ query }, res) => {
-  let { provider_id, provider_base_id, promo_id } = query;
+let allUsersTable = getUsersTable(USERS_BASE_ID);
+let getAllUsers = getAllDataFromTable(allUsersTable);
+
+let searchUser = () => {
+}
+
+let createOrUpdateUser = async ({ provider_base_id, messenger_user_id }) => {
+  let usersTable = getUsersTable(provider_base_id);
+  let getUsers = getAllDataFromTable(usersTable);
+  let createUser = createTableData(usersTable);
+
+  let filterByFormula = `{messenger user id} = '${messenger_user_id}'`;
+  let [user] = await getUsers({ filterByFormula });
+
+  if (!user) {
+    let newUserData = {};
+    let newUser = await createUser(newUserData);
+    return newUser;
+  }
   
+  return user;
+}
+
+let claimPromotion = async ({ query }, res) => {
+  let { provider_id, provider_base_id, promo_id, messenger_user_id } = query;
+
   let promosTable = getPromosTable(provider_base_id);
+
   let findPromo = findTableData(promosTable);
-  let promo = findPromo(promo_id);
-  
+
+  let promo = await findPromo(promo_id);
+
   if (!promo) {
-    let red
+    let redirect_to_blocks = [];
+    res.send({ redirect_to_blocks });
+    return;
+  }
+  
+  let updatePromoData = {
+    
   }
 }
 
