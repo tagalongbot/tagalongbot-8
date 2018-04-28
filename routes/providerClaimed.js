bf168162fe171f0004a9049ddc226491b847207a
Glitch (bevl-beauty2) <none>
2018-04-28 21:13:14 +0000

    🌪🛳 Checkpoint ./routes/providerClaimed.js:701400/726

--- routes/providerClaimed.js
+++ routes/providerClaimed.js
@@ -3,15 +3,9 @@ let { getProviderByUserID } = require('../libs/providers');
 let sendProviderClaimedMsg = async ({ query }, res) => {
   let { messenger_user_id } = query;
   let provider = getProviderByUserID(messenger_user_id);
-  
-  let redirect_to_blocks = [];
 
-  if (provider) {
-    redirect_to_blocks.push('Practice Already Claimed (By User)');
-  }
-  
-  redirect_to_blocks.push('Practice Already Claimed (Not By User)');
-
+  let block_name = (provider) ? 'Practice Already Claimed (By User)' : 'Practice Already Claimed (Not By User)';
+  let redirect_to_blocks = [block_name];
   res.send({ redirect_to_blocks });
 }
 
