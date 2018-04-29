6e304aa9fc1856ce2243f6f087bfa415f24afc21
Glitch (bevl-beauty2) <none>
2018-04-29 23:09:55 +0000

    🐸👼 Checkpoint ./routes/getProviderServices.js:701400/534

--- routes/getProviderServices.js
+++ routes/getProviderServices.js
@@ -11,20 +11,20 @@ let getServicesTable = getTable('Services');
 let servicesTable = getServicesTable(SERVICES_BASE_ID);
 let getServices = getAllDataFromTable(servicesTable);
 
-let toGalleryElement = (provider_name) => ({ id: service_id, fields: service }) => {
+let toGalleryElement = (data) => ({ id: service_id, fields: service }) => {
   let title = service['Name'];
-  let subtitle = `Service provided by ${provider_name}`.slice(0, 80);
+  let subtitle = `Service provided by ${decodeURIComponent(data.provider_name)}`.slice(0, 80);
   let image_url = service['Image URL'];
 
   let service_name = encodeURIComponent(service['Name']);
-  let read_description_btn_url = createURL(`${BASEURL}/service/description/no`, { service_id, service_name });
-  let btn1 = {
+  let read_description_btn_url = createURL(`${BASEURL}/service/description/no`, { service_id, service_name, ...data });
+  let btn = {
     title: 'Read Description',
     type: 'json_plugin_url',
     url: read_description_btn_url,
   }
 
-  let buttons = [btn1];
+  let buttons = [btn];
 
   let element = { title, subtitle, image_url, buttons };
   return element;
@@ -32,12 +32,15 @@ let toGalleryElement = (provider_name) => ({ id: service_id, fields: service }) 
 
 let getProviderListOfServices = async ({ query }, res) => {
   let { provider_id, provider_name } = query;
+  let { messenger_user_id, first_name, last_name, gender } = query;
+  let data = { messenger_user_id, first_name, last_name, gender, provider_name };
 
   let provider = await findPractice(provider_id);
   let services = await getServices();
 
-  let servicesFromProvider = services
-    .filter((service) => provider.fields['Practice Services'].includes(service.fields['Name']));
+  let servicesFromProvider = services.filter((service) => {
+    return provider.fields['Practice Services'].map(service => service.toLowerCase()).includes(service.fields['Name'].toLowerCase())
+  });
 
   if (!servicesFromProvider[0]) {
     let redirect_to_blocks = ['No Provider Services Found'];
@@ -45,7 +48,7 @@ let getProviderListOfServices = async ({ query }, res) => {
     return;
   }
 
-  let servicesGalleryData = servicesFromProvider.slice(0, 9).map(toGalleryElement(decodeURIComponent(provider_name)));
+  let servicesGalleryData = servicesFromProvider.slice(0, 9).map(toGalleryElement(data));
   let servicesGallery = createGallery(servicesGalleryData);
   let messages = [servicesGallery];
   res.send({ messages });
