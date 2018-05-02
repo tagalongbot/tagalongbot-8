47695d4d826983693d5eb7bab3654368a87d496b
Glitch (bevl-beauty2) <none>
2018-05-02 16:35:14 +0000

    🛣🌪 Checkpoint ./intents/findProvider.js:701400/54 ./intents/findProviders.js:701400/1632

--- intents/findProvider.js
+++ intents/findProvider.js
@@ -1,75 +1,53 @@
 let { BASEURL } = process.env;
-let { createTextMessage, createGallery } = require('../libs/bots');
-let { getActiveProviders } = require('../libs/data');
+let { createURL } = require('../libs/helpers');
 
-let byZipCode = (zip_code) => (provider) => {
-  if (!zip_code) return true;
-  return provider['postal'].includes(zip_code);
-}
+let findProvider = async ({ res, parameters, user }) => {
+  let { first_name, last_name, gender, messenger_user_id } = user;
+  let { brand_name, procedure, location, state, city, zip_code } = parameters;
 
-let byState = (state) => (provider) => {
-  if (!state) return true;
-  return provider['state'].toLowerCase().includes(state.toLowerCase());
-}
+  console.log('Parameters', parameters);
+  // console.log('User:', user);
 
-let byCity = (city) => (provider) => {
-  if (!city) return true;
-  return provider['city'].toLowerCase().includes(city.toLowerCase());
-}
+  let search_type = {
+    [Boolean(state)]: 'state',
+    [Boolean(city)]: 'city',
+    [Boolean(zip_code)]: 'zip_code'
+  }[true];
 
-let filterProviders = (parameters, providers) => {
-  let { provider, provider_type, state, city, zip_code, quality, location, brand_name} = parameters;
+  // console.log('Search Type:', search_type);
 
-  let filteredProviders = providers
-    .filter(byZipCode(zip_code))
-    .filter(byState(state))
-    .filter(byCity(city));
-    // .filter(byQuality(quality)
-  
-  return filteredProviders;
-}
-
-let toGalleryElement = (provider) => {
-  let title = provider.practice_name.slice(0, 80);
-  let subtitle = `${provider.first_name} ${provider.last_name} | ${provider.address}`;
-  let image_url = provider.practice_panel_photo_uri;
-
-  let btn1 = {
-    title: 'View Services',
-    type: 'json_plugin_url',
-    url: `${BASEURL}/provider/services?provider_id=${provider.providerid}&provider_name=${encodeURIComponent(provider.practice_name)}`
+  let data = {
+    search_promos_state: state,
+    search_promos_city: city,
+    search_promos_zip_code: zip_code,
   }
 
-  let btn2 = {
-    title: 'View Promos',
-    type: 'json_plugin_url',
-    url: `${BASEURL}/provider/promos?provider_id=${provider.providerid}&provider_name=${encodeURIComponent(provider.practice_name)}`
+  if ( !search_type && (brand_name || procedure) ) {
+    let redirect_to_blocks = ['Search Promos NLP (By Service)'];
+    let service_name = (brand_name || procedure).trim();
+    let set_attributes = { service_name };
+    res.send({ set_attributes, redirect_to_blocks });
+    return;
   }
 
-  let buttons = [btn1, btn2];
-
-  let element = { title, subtitle, image_url, buttons};
-  return element;
-}
-
-let findProvider = async ({ res, parameters, user}) => {
-  let { first_name } = user;
-  let activeProviders = await getActiveProviders();
-  let foundProviders = filterProviders(parameters, activeProviders);
-
-  if (!foundProviders[0]) {
-    let redirect_to_blocks = ['No Providers Found'];
+  if ( !search_type && (!brand_name && !procedure) ) {
+    let redirect_to_blocks = ['Search Promos NLP (No Procedure)'];
     res.send({ redirect_to_blocks });
     return;
   }
-  
-  let txtMsg = createTextMessage(`${first_name} here's what I found`);
-  
-  let galleryData = foundProviders.map(toGalleryElement).slice(0, 10);
-  let gallery = createGallery(galleryData);
-  
-  let messages = [txtMsg, gallery];
-  res.send({ messages });
+
+  if ( search_type && (brand_name || procedure) ) {
+    let service_name = (brand_name || procedure).toLowerCase();
+    let redirect_url = createURL(`${BASEURL}/search/promos/${search_type}`, { service_name, ...user, ...data });
+    res.redirect(redirect_url);
+    return;
+  }
+
+  if ( search_type && (!brand_name && !procedure) ) {
+    let redirect_url = createURL(`${BASEURL}/search/promos/${search_type}`, { ...user, ...data });
+    res.redirect(redirect_url);
+    return;
+  }
 }
 
 module.exports = findProvider;
