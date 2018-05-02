08dc0e12bc327bd1e35953b049ada753965f0fa4
Glitch (bevl-beauty2) <none>
2018-05-02 15:36:57 +0000

    ☔️🕶 Checkpoint ./intents/findPromos.js:701400/2802

--- intents/findPromos.js
+++ intents/findPromos.js
@@ -1,21 +1,8 @@
-let { BASEURL, SERVICES_BASE_ID, PRACTICE_DATABASE_BASE_ID } = process.env;
+let { BASEURL } = process.env;
 let { createURL } = require('../libs/helpers');
-let { searchProviders } = require('../libs/providers');
-let { toGalleryElement } = require('../libs/promos');
-let { getTable, getAllDataFromTable } = require('../libs/data');
-
-let getPracticesTable = getTable('Practices');
-let getPromosTable = getTable('Promos');
-let getServicesTable = getTable('Services');
-
-let practicesTable = getPracticesTable(PRACTICE_DATABASE_BASE_ID);
-let servicesTable = getServicesTable(SERVICES_BASE_ID);
-
-let getPractices = getAllDataFromTable(practicesTable);
-let getServices = getAllDataFromTable(servicesTable);
 
 let findPromos = async ({ res, parameters, user }) => {
-  let { first_name, last_name, messenger_user_id } = user;
+  let { first_name, last_name, gender, messenger_user_id } = user;
   let { brand_name, procedure, location, state, city, zip_code } = parameters;
 
   let search_type = {
@@ -37,65 +24,19 @@ let findPromos = async ({ res, parameters, user }) => {
     res.send({ redirect_to_blocks });
     return;
   }
-  
-  if ( search_type && (!brand_name && !procedure) ) {
-    let redirect_url = createURL(`${BASEURL}/search/promos/${search_type}`, {
-      
-    });
+
+  if ( search_type && (brand_name && procedure) ) {
+    let service_name = (brand_name || procedure).toLowerCase();
+    let redirect_url = createURL(`${BASEURL}/search/promos/${search_type}`, { service_name, ...user });
     res.redirect(redirect_url);
     return;
   }
 
-  let providers = await searchProviders({
-    search_providers_state: state, 
-    search_providers_city: city, 
-    search_providers_zip_code: zip_code, 
-  }, search_type);
-
-  if (brand_name || procedure) {
-    let filterByFormula = `OR({Capitalized Name} = '${brand_name.trim().toUpperCase()}', {Capitalized Name} = '${procedure.trim().toUpperCase()}')`;
-    let [service] = await getServices({ filterByFormula });
-
-    if (!service) {
-      let redirect_to_blocks = ['Service Not Available'];
-      res.send({ redirect_to_blocks });
-      return;
-    }
-
-    let service_name = service.fields['Name'].toLowerCase();
-    let providersWithService = providers.filter((provider) => {
-      return provider.fields['Practice Services'].map(service => service.toLowerCase()).includes(service_name);
-    });
-
-    for (let provider of providersWithService) {
-      let base_id = provider.fields['Practice Base ID'];
-      let promosTable = getPromosTable(base_id);
-      let getPromos = getAllDataFromTable(promosTable);
-
-      let filterByFormula = `OR({Type} = '${brand_name.trim().toLowerCase()}, {Type} = '${procedure.trim().toLowerCase()}')`;
-      let promos = await getPromos({ filterByFormula });
-    }
-
-  }
-  
-  
-  
-  
-  
-  
-  if (!matchingPromos[0]) {
-    let redirect_to_blocks = ['No Promos Found'];
-    res.send({ redirect_to_blocks });
+  if ( search_type && (!brand_name && !procedure) ) {
+    let redirect_url = createURL(`${BASEURL}/search/promos/${search_type}`, { ...user });
+    res.redirect(redirect_url);
     return;
   }
-
-  let txtMsg = createTextMessage(`${first_name} here's what I found`);
-
-  let galleryData = matchingPromos.map(toGalleryElement).slice(0, 5);
-  let gallery = createGallery(galleryData);
-
-  let messages = [txtMsg, gallery];
-  res.send({ messages });
 }
 
 module.exports = findPromos;
