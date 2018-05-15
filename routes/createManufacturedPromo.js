f5e4d3b66ee73fac636f171d73ec7242f3392d4d
Glitch (bevl-beauty2) <none>
2018-05-15 18:06:07 +0000

    📷🍔 Checkpoint ./routes/createManufacturedPromo.js:701400/1707

--- routes/createManufacturedPromo.js
+++ routes/createManufacturedPromo.js
@@ -8,20 +8,43 @@ let getServicesTable = getTable('Services');
 let servicesTable = getServicesTable(SERVICES_BASE_ID);
 let getServicesFromTable = getAllDataFromTable(servicesTable);
 
-let getServices = async () => {
-  let view = 'Promotions';
-  let services = await getServicesFromTable({ view });
+// let getServices = async () => {
+//   let view = 'Promotions';
+//   let services = await getServicesFromTable({ view });
   
-  let services_promos = services.map((service) => {
-    let keys = Object.keys(service).filter(key => key.toLowerCase().startsWith('promo-'));
-    
-  });
+//   let services_promos = services.map((service) => {
+//     return Object.keys(service.fields).reduce((obj, key) => {
+//       if (key.toLowerCase().startsWith('promo-')) obj[key] = service.fields[key];
+//       return { id: service.id, ...obj };
+//     }, {});
+//   });
 
-  return services_promos;
+//   return services_promos;
+// }
+
+
+let getServices = async () => {
+  let view = 'Main View';
+  let services = await getServicesFromTable({ view });
+  return services;
+}
+
+let toGalleryElement = ({ fields: service }) => {
+  let title = service['Name'];
+  let subtitle = service['Long Description'].slice(0, 80);
+  let image_url = service['Image URL'];
+  
+  let b
 }
 
 let sendManufacturedPromotions = async ({ query }, res) => {
-  
+  let services_promos = await getServices();
+  // console.log(services_promos);
+
+  let galleryData = services_promos.map(toGalleryElement);
+  let gallery = createGallery(galleryData);
+  let messages = [gallery];
+  res.send({ messages });
 }
 
 module.exports = sendManufacturedPromotions;
