9b63a92e50acb7345a8b8ac70a0e2e13b945bdce
Glitch (bevl-beauty2) <none>
2018-05-15 18:16:58 +0000

    🎿🍴 Checkpoint ./routes/createManufacturedPromo.js:701400/2726

--- routes/createManufacturedPromo.js
+++ routes/createManufacturedPromo.js
@@ -1,5 +1,5 @@
 let { BASEURL, SERVICES_BASE_ID, SURGICAL_SERVICES_IMAGE_URL } = process.env;
-let { createURL, shuffleArray } = require('../libs/helpers');
+let { createURL } = require('../libs/helpers');
 let { createGallery } = require('../libs/bots');
 
 let { getTable, getAllDataFromTable } = require('../libs/data');
@@ -22,26 +22,38 @@ let getServicesFromTable = getAllDataFromTable(servicesTable);
 //   return services_promos;
 // }
 
-
 let getServices = async () => {
   let view = 'Main View';
   let services = await getServicesFromTable({ view });
   return services;
 }
 
-let toGalleryElement = ({ fields: service }) => {
+let toGalleryElement = ({ id: service_id, fields: service }) => {
   let title = service['Name'];
-  let subtitle = service['Long Description'].slice(0, 80);
+
+  let service_types_length = Object.keys(service).filter(key => key.toLowerCase().startsWith('promo-')).length;
+  let subtitle = `${service_types_length} types of promos`;
   let image_url = service['Image URL'];
-  
-  let b
+
+  let view_service_promos_url = createURL(`${BASEURL}/promo/new/manufactured/service`, { service_id });
+
+  let btn1 = {
+    title: 'View Service Promos',
+    type: 'json_plugin_url',
+    url: view_service_promos_url
+  }
+
+  let buttons = [btn1];
+
+  let element = { title, subtitle, image_url, buttons };
+  return element;
 }
 
 let sendManufacturedPromotions = async ({ query }, res) => {
   let services_promos = await getServices();
   // console.log(services_promos);
 
-  let galleryData = services_promos.map(toGalleryElement);
+  let galleryData = services_promos.map(toGalleryElement).slice(0,1);
   let gallery = createGallery(galleryData);
   let messages = [gallery];
   res.send({ messages });
