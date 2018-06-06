54565d3aa74f1111ded4a3021c26221accad1f7d
Glitch (bevl-beauty) <none>
2018-06-06 21:10:38 +0000

    🐈🐴 Checkpoint ./routes/getServiceProviderPromos.js:701400/662

--- routes/getServiceProviderPromos.js
+++ routes/getServiceProviderPromos.js
@@ -1,3 +1,5 @@
+let { toGalleryElement } = require('../libs/promos');
+let { createMultiGallery } = require('../libs/bots');
 let { getTable, getAllDataFromTable } = require('../libs/data');
 
 let getPromosTable = getTable('Promos');
@@ -5,15 +7,21 @@ let getPromosTable = getTable('Promos');
 let getPromos = async ({ service_name, provider_base_id }) => {
   let promosTable = getPromosTable(provider_base_id);
   let view = 'Active Promos';
-  let filterByFormula = `{Type} = '${service_name.toLowerCase()}'`;
-  let promos = await getAllDataFromTable({ view, filterByFormula });
-  return promos;
+  let promos = await getAllDataFromTable({ view });
+  let matched_promos = promos.filter(
+    promo => promo.fields['Type'].toLowerCase().includes(service_name)
+  );
+
+  return matched_promos;
 }
 
 let getServiceProviderPromos = async ({ query }, res) => {
   let { service_name, provider_base_id } = query;
 
   let promos = await getPromos({ service_name, provider_base_id });
+  let galleryData = promos.map(toGalleryElement({ provider_base_id }));
+  let messages = createMultiGallery(galleryData);
+  res.send({ messages });
 }
 
 module.exports = getServiceProviderPromos;
