3bfc8805fc729114c326ff68be7212960d2c67fb
Glitch (bevl-beauty) <none>
2018-06-07 23:54:52 +0000

    📮💗 Checkpoint ./routes/claimPromotion.js:701400/732

--- routes/claimPromotion.js
+++ routes/claimPromotion.js
@@ -19,7 +19,9 @@ let getAllUsers = getAllDataFromTable(allUsersTable);
 let updateUserFromAllUsers = updateTableData(allUsersTable);
 
 // Need To Refactor and Break This Function Up
-let createOrUpdateUser = async ({ messenger_user_id, first_name, last_name, gender, user_email, provider_base_id }, provider) => {
+let createOrUpdateUser = async (data, provider) => {
+  let { messenger_user_id, first_name, last_name, gender, user_email, provider_base_id } = data;
+
   let usersTable = getUsersTable(provider_base_id);
   let getUsers = getAllDataFromTable(usersTable);
   let createUser = createTableData(usersTable);
@@ -28,6 +30,7 @@ let createOrUpdateUser = async ({ messenger_user_id, first_name, last_name, gend
   let filterByFormula = `{messenger user id} = '${messenger_user_id}'`;
   let [user] = await getUsers({ filterByFormula });
 
+  let provider_id = provider.id;
   let provider_state = provider.fields['Practice State'];
   let provider_city = provider.fields['Practice City'];
   let provider_zip_code = provider.fields['Practice Zip Code'];
@@ -43,6 +46,15 @@ let createOrUpdateUser = async ({ messenger_user_id, first_name, last_name, gend
   }
 
   let [userFromAllUsersTable] = await getAllUsers({ filterByFormula });
+
+  let provider_ids = (userFromAllUsersTable.fields['Practices Claimed Promos From'] || '').split(',');
+  
+  let new_provider_ids = [
+    ...new Set([provider_id, ...provider_ids])
+  ];
+  
+  let update
+  
   let updatedUserFromAllUsers = await updateUserFromAllUsers({ 'Email Address': user_email, ...userData }, userFromAllUsersTable);
 
   if (!user) {
