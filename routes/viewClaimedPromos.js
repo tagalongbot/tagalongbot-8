0e9a467f361c865c537ada9bad41f16dc3af4188
Glitch (bevl-beauty) <none>
2018-06-07 21:24:34 +0000

    🍞🖖 Checkpoint ./routes/viewClaimedPromos.js:701400/5201

--- routes/viewClaimedPromos.js
+++ routes/viewClaimedPromos.js
@@ -10,24 +10,36 @@ let getPromosTable = getTable('Promos');
 let { getUserByMessengerID } = require('../libs/users');
 
 let getUserFromPracticeBase = async ({ practice_base_id, messenger_user_id }) => {
-  
-}
-
-
-let getUserPromos = (messenger_user_id) => async (practice_base_id) => {
   let usersTable = getUsersTable(practice_base_id);
   let getUsers = getDataFromTable(usersTable);
 
-  let promosTable = getPromosTable(practice_base_id);
-  let getPromos = getAllDataFromTable(promosTable);
-  
   let filterByFormula = `{messenger user id} = '${messenger_user_id}'`;
   let [user] = await getUsers({ filterByFormula });
 
-  let promo_ids = user.fields['Promos Claimed'];
+  return user;
+}
+
+let getPromosFromPracticeBase = async ({ practice_base_id }) => {
+  let promosTable = getPromosTable(practice_base_id);
+  let getPromos = getAllDataFromTable(promosTable);
+
+  let view = 'Active Promos';
+  let promos = await getPromos({ view });
   
-  let view = 
-  let promos = await getPromos();
+  return promos;
+}
+
+let getUserPromos = (messenger_user_id) => async (practice_base_id) => {
+  let user = getUserFromPracticeBase({ practice_base_id, messenger_user_id });
+  let promos = getPromosFromPracticeBase({ practice_base_id, messenger_user_id });
+  
+  let promo_ids = user.fields['Promos Claimed'];
+
+  let user_promos = promos.filter(
+    promo => promo_ids.includes(promo.id)
+  );
+  
+  r
   
   
   
