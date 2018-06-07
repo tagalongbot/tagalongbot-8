f601a3d678f21b2b7a2674dc8c3689f3a76ca8b2
Glitch (bevl-beauty) <none>
2018-06-07 21:14:31 +0000

    🌚😄 Checkpoint ./routes/viewClaimedPromos.js:701400/3578

--- routes/viewClaimedPromos.js
+++ routes/viewClaimedPromos.js
@@ -2,13 +2,56 @@ let { USERS_BASE_ID } = process.env;
 
 let { createURL } = require('../libs/helpers');
 let { createMultiGallery } = require('../libs/bots');
-let { getTable, getAllDataFromTable } = require('../libs/data');
+let { getTable, getDataFromTable, getAllDataFromTable } = require('../libs/data');
 
 let getUsersTable = getTable('Users');
+let getPromosTable = getTable('Promos');
 
 let { getUserByMessengerID } = require('../libs/users');
 
-let getUserPromos = (messenger_user_id) => async () => {
+let getUserFromPracticeBase = async ({ practice_base_id, messenger_user_id }) => {
+  
+}
+
+
+let getUserPromos = (messenger_user_id) => async (practice_base_id) => {
+  let usersTable = getUsersTable(practice_base_id);
+  let getUsers = getDataFromTable(usersTable);
+
+  let promosTable = getPromosTable(practice_base_id);
+  let getPromos = getAllDataFromTable(promosTable);
+  
+  let filterByFormula = `{messenger user id} = '${messenger_user_id}'`;
+  let [user] = await getUsers({ filterByFormula });
+
+  let promo_ids = user.fields['Promos Claimed'];
+  
+  let view = 
+  let promos = await getPromos();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 }
 
@@ -17,12 +60,16 @@ let viewClaimedPromos = async ({ query }, res) => {
 
   let user = await getUserByMessengerID(messenger_user_id);
   
-  let practice_ids = user.fields['Practices Claimed Promos From'].split(',');
+  let practice_base_ids = user.fields['Practices Claimed Promos From'].split(',');
   
   let promos = Promise.all(
-    practice_ids.map(getUserPromos(messenger_user_id))
+    practice_base_ids.map(getUserPromos(messenger_user_id))
   );
-  
+
+  let galleryData = promos.map(toGalleryElement);
+
+  let messages = createMultiGallery(galleryData);
+  res.send({ messages });
 }
 
 module.exports = viewClaimedPromos;
