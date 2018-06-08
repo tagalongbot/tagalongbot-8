409b1edc87477bb536c218659300cb1b661d4aca
Glitch (bevl-beauty) <none>
2018-06-08 00:38:11 +0000

    💨🌅 Checkpoint ./routes/viewPromoId.js:701400/584 ./routes/viewClaimedPromos.js:701400/51

--- routes/viewClaimedPromos.js
+++ routes/viewClaimedPromos.js
@@ -77,12 +77,12 @@ let viewClaimedPromos = async ({ query }, res) => {
   let messenger_user_id = query['messenger user id'] || query['messenger_user_id'];
   let first_name = query['first name'] || query['first_name'];
   let last_name = query['last name'] || query['last_name'];
-  let gender1 = query['gender'] || query['gender'];
-  let data = { first_name, last_name, gender1, messenger_user_id };
+  let gender = query['gender'] || query['gender'];
+  let data = { first_name, last_name, gender1: gender, messenger_user_id };
 
   let user = await getUserByMessengerID(messenger_user_id);
 
-  let practice_ids = user.fields['Practices Claimed Promos From'].split(',');
+  let practice_ids = (user.fields['Practices Claimed Promos From'] || '').split(',');
 
   let promos = practice_ids.map(
     getUserPromos({ messenger_user_id, data })
--- routes/viewPromoId.js
+++ routes/viewPromoId.js
@@ -1,12 +1,21 @@
+let { BASEURL } = process.env;
+let { createURL } = require('../libs/helpers');
+let { createButtonMessage } = require('../libs/bots');
+
 let viewPromoId = async ({ query }, res) => {
-  let { messenger_user_id, promo_id } = query;
-  
-  let view_claimed_promos = createURL(`${BASEURL}/promo/view/claimed`, {  });
-  
+  let { messenger_user_id, first_name, last_name, gender1, promo_id} = query;
+
+  let view_claimed_promos = createURL(`${BASEURL}/promo/view/claimed`, { messenger_user_id, first_name, last_name, gender: gender1 });
+
   let msg = createButtonMessage(
     `User ID: ${messenger_user_id} \n\n Promo ID: ${promo_id}`,
-    `View Claimed Promos|json_plugin_url|${view_claimed_promos}`
+    `View Claimed Promos|json_plugin_url|${view_claimed_promos}`,
+    `Main Menu|show_block|Discover Main Menu`
   );
+
+  let messages = [msg];
+
+  res.send({ messages });
 }
 
 module.exports = viewPromoId;
