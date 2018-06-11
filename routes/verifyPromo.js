160a81d9c6ecb99b694e9fb943a83dbe6d9cd526
Glitch (bevl-beauty) <none>
2018-06-11 21:44:28 +0000

    ⛑🍼 Checkpoint ./routes/verifyPromo.js:701400/2348

--- routes/verifyPromo.js
+++ routes/verifyPromo.js
@@ -30,27 +30,46 @@ let getUserPromos = async ({ provider_base_id, user_id }) => {
   return matched_promos;
 }
 
-let toGalleryElement = ({ provider_base_id, messenger_user_id, user_messenger_id, user_record_id }) => ({ id: promo_id, fields: promo }) => {
-  let title = promo['Promotion Name'];
-  let subtitle = promo['Terms'];
-  let image_url = promo['Image URL'];
-
-  let view_promo_info_url = createURL(`${BASEURL}/promo/info`, { provider_base_id, promo_id, messenger_user_id });
+let createUpdateBtn = ({ provider_base_id, promo_id, user_messenger_id }) => {
   let update_promo_url = createURL(`${BASEURL}/promo/verify/update`, { provider_base_id, promo_id, user_messenger_id });
-
-  let btn1 = {
-    title: 'View Promo Details',
-    type: 'json_plugin_url',
-    url: view_promo_info_url,
-  }
-
   let btn2 = {
     title: 'Mark Promo As Used',
     type: 'json_plugin_url',
     url: update_promo_url,
   }
 
-  let buttons = [btn1, btn2];
+  buttons = [btn2, ...buttons];
+}
+
+let toGalleryElement = ({ provider_base_id, messenger_user_id, user_messenger_id, user_record_id }) => ({ id: promo_id, fields: promo }) => {
+  let title = promo['Promotion Name'];
+  let subtitle = promo['Terms'];
+  let image_url = promo['Image URL'];
+
+  let data = { provider_base_id, promo_id, user_messenger_id };
+  let view_promo_info_url = createURL(`${BASEURL}/promo/info`, data);
+
+  let btn1 = {
+    title: 'View Promo Details',
+    type: 'json_plugin_url',
+    url: view_promo_info_url,
+  }
+  
+  let buttons = [btn1];
+  
+  let btn2 = createUpdateBtn();
+  
+  if (btn2) buttons = [btn2,
+  
+  let buttons = createButtons(
+    data,
+    [btn1]
+  );
+  
+  let user_ids = promo.fields['Promo Used By Users'];
+  if ( !user_ids.includes(user_record_id) ) {
+    
+  }
 
   return { title, subtitle, image_url, buttons };
 }
