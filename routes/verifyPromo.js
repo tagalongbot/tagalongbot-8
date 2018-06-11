3671d3323ac20dbeb5950205111d42be7c1219f6
Glitch (bevl-beauty) <none>
2018-06-11 20:51:45 +0000

    👓🍶 Checkpoint ./routes/verifyPromo.js:701400/2443

--- routes/verifyPromo.js
+++ routes/verifyPromo.js
@@ -3,61 +3,54 @@ let { createButtonMessage } = require('../libs/bots');
 let { createURL } = require('../libs/helpers');
 let { getProviderByUserID } = require('../libs/providers');
 
-let { getTable, findTableData } = require('../libs/data');
+let { getTable, getAllDataTable, findTableData } = require('../libs/data');
 
 let getPracticesTable = getTable('Practices');
 let practicesTable = getPracticesTable(PRACTICE_DATABASE_BASE_ID);
 let findPractice = findTableData(practicesTable);
 let getPromosTable = getTable('Promos');
 
-let getPromo = async ({ provider_base_id, promo_id }) => {
+let getUserPromos = async ({ provider_base_id, user_id }) => {
   let promosTable = getPromosTable(provider_base_id);
-  let findPromo = findTableData(promosTable);
-  let promo = await findPromo(promo_id);
-  return promo;
-}
-
-let createPromoValidMsg = ({ promo, provider_base_id, query }) => {
-  let first_name = query['first name'];
-  let promo_id = promo.id;
-  let update_promo_url = createURL(`${BASEURL}/promo/verify/get_user_id`, { provider_base_id, promo_id });
-
-  let msg = createButtonMessage(
-    `${first_name} this is a valid promo, would you like to mark this promo as used`,
-    `Update Promo As Used|json_plugin_url|${update_promo_url}`,
-    `Admin Menu|show_block|Get Admin Menu`
-  );
-
-  return [msg];
-}
-
-let createPromoInvalidMsg = ({ query }) => {
-  let first_name = query['first name'];
-
-  let msg = createButtonMessage(
-    `Sorry ${first_name} this promo does not exist or already expired (Invalid Promo Code)`,
-    `Try Again|show_block|Verify Promotion`,
-    `Admin Menu|show_block|Get Admin Menu`
-  );
+  let getPromos = getAllDataTable(promosTable);
+  let view = 'Active Promos';
+  let promos = await getPromos({ view });
   
-  return [msg];
+  let matched_promos = promos.filter(
+    promo => promo.fields['Claimed By Users'].includes(user_id)
+  );
+
+  return matched_promos;
+}
+
+let toGalleryElement = ({ id: promo_id, fields: promo }) => {
+  let title = promo['Promotion Name'];
+  let subtitle = promo[''];
 }
 
 let verifyPromo = async ({ query }, res) => {
   let messenger_user_id = query['messenger user id'];
-  let promo_id = query['promo_id'];
+  let user_id = query['user_id'];
 
   let provider = await getProviderByUserID(messenger_user_id);
   let provider_base_id = provider.fields['Practice Base ID'];
 
-  let promo = await getPromo({ provider_base_id, promo_id });
+  let promos = await getUserPromos({ provider_base_id, user_id });
 
-  if (!promo) {
-    let messages = createPromoInvalidMsg({ query });
-    res.send({ messages });
-    return;
+  if (!promos[0]) {
+  
   }
 
+  
+
+
+  let messages = createPromoValidMsg({ promo, provider_base_id, query });
+  res.send({ messages });
+}
+
+module.exports = verifyPromo;
+
+/*
   if (promo.fields['Total Used'] >= promo.fields['Total Claim Count']) {
     let redirect_to_users = ['[Admin Verify Promo] Used Limit Reached'];
     let promo_claim_limit = promo.fields['Total Claim Count'];
@@ -65,9 +58,4 @@ let verifyPromo = async ({ query }, res) => {
     res.send({ redirect_to_users });
     return;
   }
-
-  let messages = createPromoValidMsg({ promo, provider_base_id, query });
-  res.send({ messages });
-}
-
-module.exports = verifyPromo;
+*/
