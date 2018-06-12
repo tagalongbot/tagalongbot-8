bc8751cfc6978075b37fef3e56b92de5e9e7a479
Glitch (bevl-beauty) <none>
2018-06-12 22:44:20 +0000

    🏗🏨 Checkpoint ./routes/admin/promos/update.js:701400/425

--- routes/admin/promos/update.js
+++ routes/admin/promos/update.js
@@ -13,18 +13,22 @@ let router = express.Router();
 let getUpdateField = ({ query }, res) => {
   let { promo_id, provider_base_id } = query;
 
-  let set_attributes = {
-    updating_promo_id: promo_id,
-    updating_provider_base_id: provider_base_id
-  }
+  let updating_promo_id = promo_id;
+  let updating_provider_base_id = provider_base_id;
 
+  let set_attributes = { updating_promo_id, updating_provider_base_id };
   let redirect_to_blocks = ['Update Promo'];
   res.send({ set_attributes, redirect_to_blocks });
 }
 
 let updatePromoInfo = async ({ query }, res) => {
-  let { update_promo_field_name, update_promo_field_value } = query;
-  let { updating_promo_id, updating_provider_base_id } = query;
+  let { 
+    updating_promo_id,
+    updating_provider_base_id,
+    update_promo_field_name, 
+    update_promo_field_value 
+  } = query;
+
   let messenger_user_id = query['messenger user id'];
   let provider = await getProviderByUserID(messenger_user_id);
 
