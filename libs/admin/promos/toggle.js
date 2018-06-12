d6e9761a74ee70f62895079800df194c73b190bc
Glitch (bevl-beauty) <none>
2018-06-12 22:33:56 +0000

    🚋🌉 Checkpoint ./routes/admin/promos/toggle.js:701400/2176 ./libs/admin/promos/toggle.js:701400/1280

--- libs/admin/promos/toggle.js
+++ libs/admin/promos/toggle.js
@@ -14,27 +14,33 @@ let getPromo = async ({ provider_base_id, promo_id }) => {
   return promo;
 }
 
-let updatePromo = async ({ provider_base_id, promo, user_record_id }) => {
+let updatePromo = async ({ provider_base_id, promo }) => {
   let promosTable = getPromosTable(provider_base_id);
   let updatePromoFromTable = updateTableData(promosTable);
 
-  let already_used_user_ids = promo.fields['Promo Used By Users'];
-  let total_used = Number(promo.fields['Total Used']);
-
-  let new_used_user_ids = [
-    ...new Set([user_record_id, ...already_used_user_ids])
-  ];
-
   let updateData = {
-    ['Promo Used By Users']: new_used_user_ids,
-    ['Total Used']: total_used + 1
+    ['Active?']: promo.fields['Active?'] ? false : true
   }
-
+  
   let updatedPromo = await updatePromoFromTable(updateData, promo);
   return updatedPromo;
+}
+
+let createUpdateMsg = ({ messenger_user_id, promo_id, provider_base_id, promo, updatedPromo }) => {
+  let toggle_promo_url = createURL(`${BASEURL}/promo/toggle`, { promo_id, provider_base_id });
+  let view_promo_details_url = createURL(`${BASEURL}/promo/view/info`, { promo_id, provider_base_id });
+  let view_active_promos_url = createURL(`${BASEURL}/promo/view/all`, { messenger_user_id });
+
+  let txtMsg = createButtonMessage(
+    `${promo.fields['Promotion Name']} is now ${updatedPromo.fields['Active?'] ? 'Active' : 'Deactivated'}`,
+    `${updatedPromo.fields['Active?'] ? 'Deactivate' : 'Reactivate'}|json_plugin_url|${toggle_promo_url}`,
+    `View Promo Details|json_plugin_url|${view_promo_details_url}`,
+    `View All Promotions|json_plugin_url|${view_active_promos_url}`
+  );
 }
 
 module.exports = {
   getPromo,
   updatePromo,
+  createUpdateMsg,
 }
--- routes/admin/promos/toggle.js
+++ routes/admin/promos/toggle.js
@@ -1,38 +1,17 @@
-let { BASEURL } = process.env;
-let { createURL } = require('../libs/helpers');
-let { createButtonMessage } = require('../libs/bots');
-let { getProviderByUserID } = require('../libs/providers');
+let { getProviderByUserID } = require('../../../libs/providers.js');
+let { getPromo, updatePromo, createUpdateMsg } = require('../../../libs/admin/promos/toggle.js');
 
 let togglePromo = async ({ query }, res) => {
   let { promo_id, provider_base_id } = query;
   let messenger_user_id = query['messenger user id'];
-  
+
   let provider = await getProviderByUserID(messenger_user_id);
-
-  let promosTable = getPromosTable(provider_base_id);
-  let findPromo = findTableData(promosTable);
-  let updatePromo = updateTableData(promosTable);
-
-  let promo = await findPromo(promo_id);
-
-  let updatePromoData = {
-    ['Active?']: promo.fields['Active?'] ? false : true
-  }
-
-  let updatedPromo = await updatePromo(updatePromoData, promo);
+  let promo = await getPromo({ provider_base_id, promo_id });
+  let updatedPromo = await updatePromo({ provider_base_id, promo });
   
-  let toggle_promo_url = createURL(`${BASEURL}/promo/toggle`, { promo_id, provider_base_id });
-  let view_promo_details_url = createURL(`${BASEURL}/promo/view/info`, { promo_id, provider_base_id });
-  let view_active_promos_url = createURL(`${BASEURL}/promo/view/all`, { messenger_user_id });
+  let updateMsg = createUpdateMsg({ messenger_user_id, promo_id, provider_base_id, promo, updatedPromo });
 
-  let txtMsg = createButtonMessage(
-    `${promo.fields['Promotion Name']} is now ${updatedPromo.fields['Active?'] ? 'Active' : 'Deactivated'}`,
-    `${updatedPromo.fields['Active?'] ? 'Deactivate' : 'Reactivate'}|json_plugin_url|${toggle_promo_url}`,
-    `View Promo Details|json_plugin_url|${view_promo_details_url}`,
-    `View All Promotions|json_plugin_url|${view_active_promos_url}`
-  );
-
-  let messages = [txtMsg];
+  let messages = [updateMsg];
   res.send({ messages });
 }
 
