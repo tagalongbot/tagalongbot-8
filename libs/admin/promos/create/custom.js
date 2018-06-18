e8c18672de3276d3d048e3c1ed3a4a73aa0b2b1c
Glitch (bevl-beauty) <none>
2018-06-18 22:50:49 +0000

    🐪🐪 Checkpoint ./routes/admin/promos/create/custom.js:701400/921 ./libs/admin/promos/create/custom.js:701400/1060

--- libs/admin/promos/create/custom.js
+++ libs/admin/promos/create/custom.js
@@ -1,7 +1,25 @@
+let { BASEURL } = process.env;
 let { createURL } = require('../../../../libs/helpers.js');
 let { createBtn } = require('../../../../libs/bots.js');
 
-let toServicesGallery = ({ id: service_id, fields: service }) => {
+let toServicesGallery = ({ messenger_user_id, new_promo_name, new_promo_expiration_date, new_promo_claim_limit }) => ({ id: service_id, fields: service }) => {
+  let title = service['Name'];
+  let image_url = service['Image URL'];
+
+  let send_images_url = createURL(
+    `${BASEURL}/admin/promos/create/custom/images`,
+    { messenger_user_id, service_id, new_promo_name, new_promo_expiration_date, new_promo_claim_limit }
+  );
+
+  let btn1 = createBtn(`View Service Images|json_plugin_url|${send_images_url}`);
+
+  let buttons = [btn1];
+
+  return { title, image_url, buttons };
+}
+
+let toImagesGallery = (new_promo_name, new_promo_expiration_date, new_promo_claim_limit) => ({ id: promo_id, fields: promo }) => {
+  let title = n
   
 }
 
--- routes/admin/promos/create/custom.js
+++ routes/admin/promos/create/custom.js
@@ -1,7 +1,7 @@
 let { createMultiGallery } = require('../../../../libs/bots.js');
 let { getProviderByUserID } = require('../../../../libs/data/providers.js');
 let { findService, getAllServices, filterServicesFromProvider } = require('../../../../libs/data/services.js');
-let { toServicesGallery, toImag } = require('../../../../libs/admin/promos/create/custom.js');
+let { toServicesGallery, toImagesGallery } = require('../../../../libs/admin/promos/create/custom.js');
 let { getCustomPromosByService } = require('../../../../libs/data/custom-promos.js');
 let { createNewPromo } = require('../../../../libs/admin/promos/create/custom/confirm.js');
 
@@ -32,9 +32,21 @@ let sendCustomImages = async ({ query }, res) => {
   let service = await findService(service_id);
   let service_name = service.fields['Name'];
   let custom_promos = await getCustomPromosByService({ service_name });
-  
-  
-  
+
+  let galleryData = custom_promos.map(
+    toImagesGallery({ new_promo_name, new_promo_expiration_date, new_promo_claim_limit })
+  );
+
+  let messages = createMultiGallery(galleryData);
+  res.send({ messages });
+}
+
+let sendSelectedImage = async ({ query }, res) => {
+  let {} = query;
+
+  let set_attributes = {  };
+  let redirect_to_blocks = ['New Custom Promotion Confirmation'];
+  res.send({ set_attributes, redirect_to_blocks });
 }
 
 let createCustomPromo = async ({ query }, res) => {
@@ -59,6 +71,7 @@ let createCustomPromo = async ({ query }, res) => {
 
 router.get('/services', sendProviderServices);
 router.get('/images', sendCustomImages);
+router.get('/images/select', sendSelectedImage);
 router.get('/confirm', createCustomPromo);
 
 module.exports = router;
