0c673422f383141653596ba1a5b202d992fbd81a
Glitch (bevl-beauty) <none>
2018-06-18 21:05:24 +0000

    🙍🍟 Checkpoint ./libs/admin/promos/create/manufactured.js:701400/612

--- libs/admin/promos/create/manufactured.js
+++ libs/admin/promos/create/manufactured.js
@@ -36,14 +36,25 @@ let getServicesWithPromos = async ({ services }) => {
 
 let createExpirationDate = (new_expiration_date_str) => {
   let dateMap = {
-    '1 Weeks': 7,
-    '2 Weeks': 14
+    '1 Week': 7,
+    '2 Weeks': 14,
+    '3 Weeks': 21,
+    '4 Weeks': 28,
+    '5 Weeks': 35,
+    '6 Weeks': 42,
+    '7 Weeks': 49,
+    '8 Weeks': 56,
+    '9 Weeks': 63,
+    '10 Weeks': 70,
   }
-  
+
   let today = new Date();
-  let new_date = 
-  
-  let new_date = 
+
+  let new_date = (new Date()).setDate(
+    today.getDate() + dateMap[new_expiration_date_str]
+  );
+
+  return new_date;
 }
 
 let createNewPromo = async (data) => {
@@ -58,7 +69,7 @@ let createNewPromo = async (data) => {
   let new_promo_type = manufactured_promo.fields['Name'];
 
   let new_promo_image = manufactured_promo.fields['Image URL'];
-  let expiration_date = new Date(new_promo_expiration_date);
+  let expiration_date = createExpirationDate(new_promo_expiration_date);
 
   let promoData = {
     ['Promotion Name']: `${new_promo_type} on ${service.fields['Name']}`,
