c90288ab454fce55cf4564bfd0447317c50ad6e8
Glitch (bevl-beauty) <none>
2018-06-18 20:23:30 +0000

    🛀🎠 Checkpoint ./routes/services/promos.js:701400/2002

--- routes/services/promos.js
+++ routes/services/promos.js
@@ -35,3 +35,64 @@ let getServicePromos = async ({ query }, res) => {
 }
 
 module.exports = getServicePromos;
+
+
+let express = require('express');
+let router = express.Router();
+
+let searchServicePromos = async ({ query }, res) => {
+  let { service_id } = query;
+  let service = await findService(service_id);
+  let service_name = service.fields['Name'];
+
+  let set_attributes = { service_id, service_name };
+  let redirect_to_blocks = ['Search Service Promos'];
+  res.send({ set_attributes, redirect_to_blocks });
+}
+
+let getServicePromos = async ({ query, params }, res) => {
+  let { search_type } = params;
+
+  let { messenger_user_id, first_name, last_name, gender } = query;
+  let { service_id, search_service_promos_state, search_service_promos_city, search_service_promos_zip_code } = query;
+
+  let service = await findService(service_id);
+  let service_name = service.fields['Name'];
+
+  let providers = await getProviders(
+    { search_type, search_service_providers_state, search_service_providers_city, search_service_providers_zip_code }
+  );
+
+  let providersByService = (providers[0]) ? filterProvidersByService(service_name, providers) : [];
+
+  if (!providersByService[0]) {
+    let set_attributes = { service_name }
+    let redirect_to_blocks = ['No Service Providers Found'];
+    res.send({ set_attributes, redirect_to_blocks });
+    return;
+  }
+
+  let randomProviders = shuffleArray(providersByService).slice(0, 9).sort(sortProviders).map(
+    toGalleryElement({ first_name, last_name, gender, messenger_user_id })
+  );
+
+  let last_gallery_element = createLastGalleryElement();
+	let providersGallery = createGallery([...randomProviders, last_gallery_element], 'square');
+  let messages = [providersGallery];
+  res.send({ messages });
+}
+
+router.get('/', searchServicePromos);
+router.get('/:search_type', getServicePromos);
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
