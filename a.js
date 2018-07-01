4f47c1f85ddb2227c975d76f4dc68eb6c807b207
Glitch (bevl-beauty) <none>
2018-07-01 22:16:03 +0000

    🌻👴 Checkpoint ./a.js:701400/234

--- a.js
+++ a.js
@@ -1,3 +1,9 @@
-let someFunction = ({ practice_id }) => {}
+let func1 = ({ practice_id }) => {}
+let func2 = ({ practice_id }) => {}
 
-let b = someFunction({ practice_id  });
+let practice_id = '';
+
+let a = func1({ practice_id });
+let b = func2({ practice_id });
+
+
