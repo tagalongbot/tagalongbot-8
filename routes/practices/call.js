fc8290c8cc8f28770631807eba5ef176e9205387
Glitch (bevl-beauty) <none>
2018-07-03 22:35:14 +0000

    🌁🐋 Checkpoint ./routes/practices/call.js:701400/99

--- routes/practices/call.js
+++ routes/practices/call.js
@@ -36,7 +36,6 @@ let callPractice = async ({ query }, res) => {
   let { practice_id, messenger_user_id: user_messenger_id } = query;
 
   let practice = await getPracticeByID(practice_id);
-  console.log('practice', practice);
   let practice_name = practice.fields['Practice Name'];
 
   let user = await getUserByMessengerID(user_messenger_id);
