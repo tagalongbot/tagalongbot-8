5ceb55daef8c9c40bfbdbcab1b93836aa683f107
Glitch (bevl-beauty) <none>
2018-07-03 22:03:31 +0000

    🚟☂️ Checkpoint ./routes/files.js:701400/266 ./routes/practices/call.js:701400/184

--- routes/files.js
+++ routes/files.js
@@ -1,14 +1,6 @@
-let fs = require('fs');
-let { promisify } = require('util');
-
-let readFile = promisify(fs.readFile);
-
 let getFile = async ({ query }, res) => {
-  console.log('query', query);
   let { file_path } = query;
-  let file = await readFile(`.data/${file_path}`);
-  console.log('file', file);
-  res.send(file);
+  res.sendFile(`${file_path}`, { root: '.data' });
 }
 
 module.exports = getFile;
