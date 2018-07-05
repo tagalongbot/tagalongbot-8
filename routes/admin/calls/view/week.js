a3672f920b404d39aecab9277e61d7d609bf503e
Glitch (bevl-beauty) <none>
2018-07-05 21:28:39 +0000

    🏯🎋 Checkpoint ./tags/calls/week-calls.tag:701400/976 ./routes/admin/calls/view/week.js:701400/802

--- routes/admin/calls/view/week.js
+++ routes/admin/calls/view/week.js
@@ -1,5 +1,29 @@
-let getCallsThisWeek = async () => {
+let riot = require('riot');
+let week_tag = require('../../../../tags/calls/week-calls.tag');
+
+let { getPracticeCalls } = require('../../../../libs/data/practice/calls.js');
+
+let filterCallsThisWeek = ({ calls }) => {
   
+}
+
+let toCallData = ({ fields: call }) => {
+  let call_name = 
+  
+  let new_call = {};
+  return new_call;
+}
+
+let getCallsThisWeek = async () => {
+  let all_practice_calls = await getPracticeCalls();
+
+  let calls_this_week = filterCallsThisWeek(
+    { calls: all_practice_calls }
+  );
+  
+  let calls = calls_this_week.map(
+    
+  );
 }
 
 module.exports = getCallsThisWeek;
---
+++ tags/calls/week-calls.tag
@@ -0,0 +1,15 @@
+<week-calls>
+  <div class="row" each="{ call in opts.calls }">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">{ call.caller_name }</span>
+          <p>Call By { call.caller_name }</p>
+        </div>
+        <div class="card-action">
+          <a href="{ call.call_url }">Call { call.caller_first_name } Back</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</week-calls>
