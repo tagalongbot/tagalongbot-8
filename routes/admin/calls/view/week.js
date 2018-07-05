ca3f4e3efde9f95a5fbb752b212099425e9c6043
Glitch (bevl-beauty) <none>
2018-07-05 21:50:54 +0000

    💓✊ Checkpoint ./routes/admin/calls/view/week.js:701400/520

--- routes/admin/calls/view/week.js
+++ routes/admin/calls/view/week.js
@@ -7,7 +7,15 @@ let { getPracticeByUserID } = require('../../../../libs/data/practices.js');
 let { getPracticeCalls } = require('../../../../libs/data/practice/calls.js');
 
 let filterCallsThisWeek = ({ calls }) => {
-  
+  let today = new Date();
+
+  let calls_this_week = calls.map((call) => {
+    let call_date = call.fields['Date / Time Created'];
+    console.log('call_date', call_date);
+    return true;
+  });
+
+  return calls_this_week;
 }
 
 let toCallData = ({ fields: call }) => {
@@ -24,11 +32,12 @@ let toCallData = ({ fields: call }) => {
 
 let getCallsThisWeek = async ({ query }, res) => {
   let { messenger_user_id } = query;
-  
+
   let practice = await getPracticeByUserID(messenger_user_id);
   let practice_calls_base_id = practice.fields['Practice Calls Base ID'];
 
   let all_practice_calls = await getPracticeCalls({ practice_calls_base_id });
+  console.log('all_practice_calls', all_practice_calls);
 
   let calls_this_week = filterCallsThisWeek(
     { calls: all_practice_calls }
@@ -37,7 +46,7 @@ let getCallsThisWeek = async ({ query }, res) => {
   let calls = calls_this_week.map(
     toCallData
   );
-  
+
   let view_html = riot.render(week_tag, { calls });
   res.render('week-calls', { view_html });
 }
