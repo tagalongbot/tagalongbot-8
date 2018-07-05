cec104eedc80a760142e37e88acab89e951a2ef9
Glitch (bevl-beauty) <none>
2018-07-05 22:12:09 +0000

    😽🕵 Checkpoint ./routes/admin/calls/view/week.js:701400/1968

--- routes/admin/calls/view/week.js
+++ routes/admin/calls/view/week.js
@@ -7,8 +7,19 @@ let week_tag = require('../../../../tags/calls/week-calls.tag');
 
 let { getPracticeByUserID } = require('../../../../libs/data/practices.js');
 let { getPracticeCalls } = require('../../../../libs/data/practice/calls.js');
+let { getPracticeUser, getUserPromos } = require('../../../../libs/data/practice/users.js');
 
-let toCallData = ({ fields: call }) => {
+let toCallData = ({ practice_users_base_id }) => async ({ fields: call }) => {
+  let user_messenger_id = call['messenger user id'];
+
+  let practice_user = await getPracticeUser(
+    { practice_users_base_id, user_messenger_id }
+  );
+  
+  let practice_user_promos = await getUserPromos(
+    { practice_users_base_id, user_id: user_messenger_id }
+  );
+
   let caller_first_name = call['First Name'];
   let caller_last_name = call['Last Name'];
 
@@ -28,14 +39,17 @@ let getCallsThisWeek = async ({ query }, res) => {
 
   let practice = await getPracticeByUserID(messenger_user_id);
   let practice_calls_base_id = practice.fields['Practice Calls Base ID'];
+  let practice_users_base_id = practice.fields['Practice Users Base ID'];
 
   let calls_this_week = await getPracticeCalls(
     { practice_calls_base_id, view: 'Calls This Week' }
   );
 
-  let calls = calls_this_week.map(
-    toCallData
-  );
+  let calls = await Promise.all(calls_this_week.map(
+    toCallData({ practice_users_base_id })
+  ));
+
+  console.log('calls', calls);
 
   let view_html = riot.render(week_tag, { calls });
   res.render('week-calls', { view_html });
