cd8ff670a0b9f574d69c49f85321a8cc26c919a6
Glitch (bevl-beauty) <none>
2018-07-09 19:49:31 +0000

    🏌🚔 Checkpoint ./libs/data/practice/users.js:701400/513 ./libs/data/users.js:701400/663

--- libs/data/users.js
+++ libs/data/users.js
@@ -31,9 +31,31 @@ let updateUser = async (userData, user) => {
   return updatedUser;
 }
 
+let getUserPromos = async ({ user_id, view = 'Main View' }) => {
+  let user = await findUser(user_id);
+  
+  let c
+  
+  let promosTable = getPromosTable(practice_promos_base_id);
+  let getPromos = getAllDataFromTable(promosTable);
+
+  let promos = await getPromos({ view });
+
+  let matched_promos = promos.filter((promo) => { 
+    let promo_claimed_by_users = convertLongTextToArray(
+      promo.fields['Claimed By Users']
+    );
+    
+    return promo_claimed_by_users.includes(user_id);
+  });
+  
+  return matched_promos;
+}
+
 module.exports = {
   getUserByID,
   getUserByMessengerID,
   createUser,
   updateUser,
+  getUserPromos,
 }
