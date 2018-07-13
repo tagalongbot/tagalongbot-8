988d8268eace7170103b509ec66f397e8c84be1b
Glitch (bevl-beauty) <none>
2018-07-13 23:21:32 +0000

    🎺🚛 Checkpoint ./routes/promos/broadcasts.js:701400/437

--- routes/promos/broadcasts.js
+++ routes/promos/broadcasts.js
@@ -2,18 +2,23 @@ let { getUserByMessengerID } = require('../../libs/data/users.js');
 
 let getPromos = require('../../routes/promos/promos.js');
 
-let findPromos = async ({ query, params }, res) => {
+let findPromosOffLastSearch = async ({ query, params }, res) => {
   let { search_type } = params;
   let { messenger_user_id } = query;
 
   let user = await getUserByMessengerID(messenger_user_id);
-
+  let first_name = user.fields['First Name'];
+  let last_name = user.fields['Last Name'];
+  let gender = user.fields['Gender'];
+  
   let search_promos_state = (search_type === 'state') ? user.fields['Last State Searched'] : null;
   let search_promos_city = (search_type === 'state') ? user.fields['Last City Searched'] : null;
+  
+  let new_query = { messenger_user_id, first_name, last_name, gender, search_promos_state, search_promos_city };
 
   getPromos(
-    { search_promos_state, search_promos_city }, res
+    { query: new_query }, res
   );
 }
 
-module.exports = findPromos;
+module.exports = findPromosOffLastSearch;
