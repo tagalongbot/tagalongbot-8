b568a590027ad72556e9a614d3a689ca2c434eb1
Glitch (bevl-beauty) <none>
2018-07-18 18:13:32 +0000

    📇🐇 Checkpoint ./middlewares/handleRoute.js:701400/266

--- middlewares/handleRoute.js
+++ middlewares/handleRoute.js
@@ -3,13 +3,12 @@ let { sendErrorMsg } = require('../libs/telegram.js');
 let { logToFile } = require('../libs/helpers.js');
 
 let errorHandler = (block_name, res) => async (error) => {
-  console.log(error, Object.keys(error));
+  console.log(error);
   logToFile(`${block_name}`, 'errors.txt');
+  sendErrorMsg(block_name);
+
   let redirect_to_blocks = [block_name];
   res.send({ redirect_to_blocks });
-  sendErrorMsg(
-    JSON.stringify(error)
-  );
 }
 
 let handleRoute = (routeFn, block_name) => (req, res) => {
