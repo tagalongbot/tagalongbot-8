ae41e1086249aef192673088c0b5c0203dd73795
Glitch (bevl-beauty) <none>
2018-07-20 00:17:39 +0000

    💵🌺 Checkpoint ./libs/practices/practices.js:701400/2956 ./routes/practices.js:701400/270

--- libs/practices/practices.js
+++ libs/practices/practices.js
@@ -9,10 +9,8 @@ let { getPracticesByState, getPracticesByCity } = require('../../libs/data/pract
 let createNewUserData = (data) => {
   let { messenger_user_id, first_name, last_name, gender } = data;
 
-  let { search_practices_state, search_practices_city, search_practices_zip_code } = data;
+  let { search_practices_zip_code } = data;
 
-  let last_state_searched = search_practices_state ? search_practices_state.trim().toLowerCase() : null;
-  let last_city_searched = search_practices_city ? search_practices_city.trim().toLowerCase() : null;
   let last_zip_code_searched = search_practices_zip_code ? Number(search_practices_zip_code.trim()) : null;
 
   let new_user_data = {
@@ -21,81 +19,43 @@ let createNewUserData = (data) => {
     'First Name': first_name,
     'Last Name': last_name,
     'Gender': gender,
-    'Last State Searched': last_state_searched,
-    'Last City Searched': last_city_searched,
     'Last Zip Code Searched': last_zip_code_searched,
   }
 
   return new_user_data;
 }
 
-let createUpdateUserData = ({ search_practices_state, search_practices_city, search_practices_zip_code }) => {
+let createUpdateUserData = ({ search_practices_zip_code }) => {
   let update_user_data = {};
 
-  let last_state_searched = search_practices_state ? search_practices_state.trim().toLowerCase() : null;
-  let last_city_searched = search_practices_city ? search_practices_city.trim().toLowerCase() : null;
   let last_zip_code_searched = search_practices_zip_code ? Number(search_practices_zip_code.trim()) : null;
 
-  if (last_state_searched) update_user_data['Last State Searched'] = last_state_searched;
-  if (last_city_searched) update_user_data['Last City Searched'] = last_city_searched;
   if (last_zip_code_searched) update_user_data['Last Zip Code Searched'] = last_zip_code_searched;
 
   return update_user_data;  
 }
 
-let createButtons = (practice, data) => {
+let createButtons = (data) => {
   let { practice_id, first_name, last_name, gender, messenger_user_id } = data;
 
-  let is_practice_active = practice['Active?'];
-  let is_practice_claimed = practice['Claimed?'];
+  let view_services_btn_url = createURL(
+    `${BASEURL}/practices/services`,
+    { practice_id, first_name, last_name, gender, messenger_user_id }
+  );
 
-  if (is_practice_active) {
-    let view_services_btn_url = createURL(
-      `${BASEURL}/practices/services`,
-      { practice_id, first_name, last_name, gender, messenger_user_id }
-    );
+  let view_promos_btn_url = createURL(
+    `${BASEURL}/practices/promos`,
+    { practice_id, first_name, last_name, gender, messenger_user_id }  
+  );
 
-    let view_promos_btn_url = createURL(
-      `${BASEURL}/practices/promos`,
-      { practice_id, first_name, last_name, gender, messenger_user_id }  
-    );
+  let btn1 = createBtn(`View Services|json_plugin_url|${view_services_btn_url}`);
+  let btn2 = createBtn(`View Promos|json_plugin_url|${view_promos_btn_url}`);
 
-    let btn1 = createBtn(`View Services|json_plugin_url|${view_services_btn_url}`);
-    let btn2 = createBtn(`View Promos|json_plugin_url|${view_promos_btn_url}`);
-
-    return [btn1, btn2];
-  }
-
-  if (!is_practice_claimed) {
-    let claim_practice_url = createURL(
-      `${BASEURL}/practices/claim/user_info`,
-      { practice_id, first_name, last_name, gender, messenger_user_id }
-    );
-
-    let btn = createBtn(`Claim Practice|json_plugin_url|${claim_practice_url}`);
-
-    return [btn];
-  }
-
-  if (is_practice_claimed && !is_practice_active) {
-    let { messenger_user_id } = data;
-
-    let already_claimed_url = createURL(
-      `${BASEURL}/practices/claimed`,
-      { messenger_user_id }
-    );
-
-    let btn = createBtn(`Already Claimed|json_plugin_url|${already_claimed_url}`);
-
-    return [btn];
-  }
+  return [btn1, btn2];
 }
 
 // Booking Site and Site URL Only
 let createButtons2 = (practice, data) => {
-  let is_practice_active = practice['Active?'];
-  let is_practice_claimed = practice['Claimed?'];
-
   let view_practice_site_url = practice['Practice Website'];
   let view_practice_book_url = practice['Practice Booking URL'];
 
@@ -117,11 +77,11 @@ let createButtons2 = (practice, data) => {
 // Exported Functions
 let createOrUpdateUser = async (user, query) => {
   let { messenger_user_id, first_name, last_name, gender } = query;
-  let { search_practices_state, search_practices_city, search_practices_zip_code } = query;
+  let { search_practices_zip_code } = query;
 
   if (!user) {
     let new_user_data = createNewUserData(
-      { search_practices_state, search_practices_city, search_practices_zip_code, messenger_user_id, first_name, last_name, gender }
+      { search_practices_zip_code, messenger_user_id, first_name, last_name, gender }
     );
 
 		let newUser = await createUser(new_user_data);
@@ -129,7 +89,7 @@ let createOrUpdateUser = async (user, query) => {
 	}
 
   let update_user_data = createUpdateUserData(
-    { search_practices_state, search_practices_city, search_practices_zip_code }
+    { search_practices_zip_code }
   );
 
   let updatedUser = await updateUser(update_user_data, user);
@@ -144,7 +104,6 @@ let toGalleryElement = (data) => ({ id: practice_id, fields: practice }) => {
   let image_url = practice['Main Provider Image'] ? practice['Main Provider Image'][0].url : DEFAULT_PRACTICE_IMAGE;
 
   let buttons = createButtons(
-    practice,
     { practice_id, first_name, last_name, gender, messenger_user_id }
   );
 
--- routes/practices.js
+++ routes/practices.js
@@ -7,11 +7,8 @@ let cache = require('../middlewares/cache.js');
 let getPractices = require('../routes/practices/practices.js');
 let getPracticeServices = require('../routes/practices/services.js');
 let getPracticePromos = require('../routes/practices/promos.js');
-let claimPractice = require('../routes/practices/claim.js');
-let practiceClaimed = require('../routes/practices/claimed.js');
 let listPractice = require('../routes/practices/list.js');
 let callPractice = require('../routes/practices/call.js');
-
 
 router.get(
   '/search/:zip_code',
@@ -27,16 +24,6 @@ router.get(
 router.get(
   '/promos/:zip_code',
   handleRoute(getPracticePromos, '[Error] Viewing Practice Promos')
-);
-
-router.use(
-  '/claim',
-  claimPractice,
-);
-
-router.get(
-  '/claimed',
-  handleRoute(practiceClaimed, '[Error] Checking Claimed Practice')
 );
 
 router.use(
