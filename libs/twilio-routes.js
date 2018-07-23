e26e2fc040b571be3b0a5bba08f4ab6bce7f8767
Glitch (bevl-beauty) <none>
2018-07-23 18:53:44 +0000

    🏑🚠 Checkpoint ./libs/twilio-routes.js:701400/234

---
+++ libs/twilio-routes.js
@@ -0,0 +1,12 @@
+let handleVerifyPhoneNumberRoute = ({ phone_number, block_name }) => {
+  
+}
+
+let handleVerifyVerificationCode = () => {
+  
+}
+
+module.exports = {
+  handleVerifyPhoneNumberRoute,
+  handleVerifyVerificationCode,
+}
