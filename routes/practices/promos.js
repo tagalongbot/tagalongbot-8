8935625e40f9137340389ecb3fdaa3b9508d44d7
Glitch (bevl-beauty) <none>
2018-07-25 22:54:56 +0000

    💔🔨 Checkpoint ./routes/practices/promos.js:701400/138

--- routes/practices/promos.js
+++ routes/practices/promos.js
@@ -11,6 +11,7 @@ let getPracticesPromos = async ({ query }, res) => {
   let { practice_id, messenger_user_id, first_name, last_name, gender } = query;
   
   let practice = await getPracticeByID(practice_id);
+  let practice_name = practice.fields['Practice Name'];
   let practice_promos_base_id = practice.fields['Practice Promos Base ID'];
 
   let promos = await getPracticePromos(
@@ -18,8 +19,9 @@ let getPracticesPromos = async ({ query }, res) => {
   );  
 
   if (!promos[0]) {
+    let set_attributes = { practice_name };
     let redirect_to_blocks = ['No Practice Promos Found'];
-    res.send({ redirect_to_blocks });
+    res.send({ set_attributes, redirect_to_blocks });
     return;
   }
 
