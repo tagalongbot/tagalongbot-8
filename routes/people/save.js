4f81adc10d5de20b7e8b2ccde949fdb38898d9f7
Glitch (the3dwin-tag-along) <none>
2018-10-06 18:59:20 +0000

    👊⏳ Checkpoint ./routes/people/save.js:701400/3063

---
+++ routes/people/save.js
@@ -0,0 +1,17 @@
+let { getPersonByMessengerID, createPerson } = require('../../libs/data/people.js');
+
+let { uploadCloudinaryImage, getFaceFromImage } = require('../../libs/cloudinary.js');
+
+let savePerson = async ({ query }, res) => {
+  let { messenger_user_id } = query;
+
+  let new_person_data = {
+    ['messenger user id']: messenger_user_id,
+  }
+
+  let new_person = await createPerson(new_person_data);
+
+  res.sendStatus(200);
+}
+
+module.exports = savePerson;
