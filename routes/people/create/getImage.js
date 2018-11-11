25c19592367776bb125c0b857c2c983868b1bf39
Glitch (the3dwin-tag-along) <none>
2018-11-11 22:15:15 +0000

    ✍️⛪️ Checkpoint ./routes/people/create/getImage.js:701400/963

--- routes/people/create/getImage.js
+++ routes/people/create/getImage.js
@@ -1,6 +1,8 @@
 let { getPersonByMessengerID, updatePerson } = require('../../../libs/data/people.js');
 
-let { uploadCloudinaryImage, getFaceFromImage, createFaceImage } = require('../../../libs/cloudinary.js');
+let { uploadCloudinaryImage, getFaceFromImage } = require('../../../libs/cloudinary.js');
+
+let { createImage, createBtn, createQuickReplyMessage } = require();
 
 let getImage = async ({ query }, res) => {
   let { profile_pic_url } = query;
@@ -9,21 +11,26 @@ let getImage = async ({ query }, res) => {
     { image_url: profile_pic_url }
   );
 
-  console.log('new_profile_image_url', new_profile_image_url);
-
   let face_profile_image_url = getFaceFromImage(
     { image_url: new_profile_image_url }
   );
 
-  console.log('face_profile_image_url', face_profile_image_url);
+  let set_attributes = {
+    profile_image: face_profile_image_url
+  }
 
-  let face_image = createFaceImage(
-    { image_url: profile_pic_url, width: 200, height: 200 }
+  let image_msg = createImage(face_profile_image_url);
+
+  let quick_reply_1 = createBtn();
+
+  let quick_replies = createQuickReplyMessage(
+    `Is this a good image to use for your profile?`,
+    
   );
 
-  let redirect_to_blocks = ['New Profile Created'];
+  let messages = [];
 
-  res.send({ redirect_to_blocks });
+  res.send({ set_attributes, messages });
 }
 
 module.exports = getImage;
