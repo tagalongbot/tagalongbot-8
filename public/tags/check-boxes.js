3b514c107d4cbf4c97576c8a14f85664e5186a80
Glitch (the3dwin-tag-along) <none>
2018-12-10 22:35:28 +0000

    🎭😀 Checkpoint ./tags/check-boxes.tag:701400/301 ./app.js:701400/6 ./routes/people/view/interests.js:701400/7

--- public/tags/check-boxes.js
+++ public/tags/check-boxes.js
@@ -1,6 +1,12 @@
-riot.tag2('check-boxes', '<h3 class="center-align">{opts.title}</h3> <p each="{opts.options}"> <label> <input if="{checked == false}" type="checkbox" class="filled-in"> <input if="{checked == true}" type="checkbox" class="filled-in" checked="checked"> <span>{label}</span> </label> </p> <button ref="thebutton" class="btn waves-effect waves-light" type="submit" name="action">Submit <i class="material-icons right">send</i> </button>', '', '', function(opts) {
+riot.tag2('check-boxes', '<h3 class="center-align">{opts.title}</h3> <p each="{opts.options}"> <label> <input if="{checked == false}" type="checkbox" class="filled-in" onclick="{parent.toggle}"> <input if="{checked == true}" type="checkbox" class="filled-in" checked="checked" onclick="{parent.toggle}"> <span>{label}</span> </label> </p> <button ref="thebutton" class="btn waves-effect waves-light" type="submit" name="action">Submit <i class="material-icons right">send</i> </button>', '', '', function(opts) {
     let self = this;
     self.options = opts.options;
+
+    self.toggle = function (e) {
+      console.log('toggled', e);
+      var item = e.item;
+      item.checked = !item.checked;
+    }
 
     let onFormSubmit = function(evt) {
       evt.preventDefault();
--- tags/check-boxes.tag
+++ tags/check-boxes.tag
@@ -3,8 +3,8 @@
   <h3 class="center-align">{ opts.title }</h3>
   <p each="{ opts.options }">
     <label>
-      <input if="{ checked == false }" type="checkbox" class="filled-in" />
-      <input if="{ checked == true }" type="checkbox" class="filled-in" checked="checked" />
+      <input if="{ checked == false }" type="checkbox" class="filled-in" onclick="{ parent.toggle }" />
+      <input if="{ checked == true }" type="checkbox" class="filled-in" checked="checked" onclick="{ parent.toggle }" />
       <span>{ label }</span>
     </label>
   </p>
@@ -16,13 +16,17 @@
     let self = this;
     self.options = opts.options;
 
+    self.toggle = function (e) {
+      var item = e.item;
+      item.checked = !item.checked;
+    }
+
     let onFormSubmit = function(evt) {
       evt.preventDefault();
       console.log('options', self.options);
     }
 
     self.on('mount', function(eventName) {
-      console.log('mounted');
       self.refs.thebutton.onclick = onFormSubmit;
     });
   </script>
