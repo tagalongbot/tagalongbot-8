6ebeefd66d2dc6ae13c97a4da869122a39b5a280
Glitch (the3dwin-tag-along) <none>
2018-12-11 22:37:58 +0000

    🎬💎 Checkpoint ./tags/check-boxes.tag:701400/1779 ./app.js:701400/2 ./package.json:701400/22950

--- views/check-boxes.marko.js
+++ views/check-boxes.marko.js
@@ -1,8 +1,8 @@
-// Compiled using marko@4.13.8 - DO NOT EDIT
+// Compiled using marko@4.14.3 - DO NOT EDIT
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/bevlbeauty$0.0.1/views/check-boxes.marko",
+    marko_componentType = "/tagalong$0.0.1/views/check-boxes.marko",
     components_helpers = require("marko/src/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
@@ -46,7 +46,7 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/bevlbeauty$0.0.1/views/check-boxes.marko",
+    id: "/tagalong$0.0.1/views/check-boxes.marko",
     tags: [
       "marko/src/components/taglib/component-globals-tag",
       "marko/src/components/taglib/init-components-tag",
--- views/user-profile.marko.js
+++ views/user-profile.marko.js
@@ -1,8 +1,8 @@
-// Compiled using marko@4.13.8 - DO NOT EDIT
+// Compiled using marko@4.14.3 - DO NOT EDIT
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/bevlbeauty$0.0.1/views/user-profile.marko",
+    marko_componentType = "/tagalong$0.0.1/views/user-profile.marko",
     components_helpers = require("marko/src/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
@@ -38,7 +38,7 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/bevlbeauty$0.0.1/views/user-profile.marko",
+    id: "/tagalong$0.0.1/views/user-profile.marko",
     tags: [
       "marko/src/components/taglib/component-globals-tag",
       "marko/src/components/taglib/init-components-tag",
