86d14d6a215c0a35368eb8c8da5b92192d250e74
Glitch (the3dwin-tag-along) <none>
2018-12-15 22:41:54 +0000

    🚛🌿 Checkpoint ./routes/people/update/image/upload.js:701400/673

--- routes/people/update/image/upload.js
+++ routes/people/update/image/upload.js
@@ -1,6 +1,6 @@
 let { getPersonByMessengerID, updatePerson } = require('../../../../libs/data/people.js');
 
-let { uploadCloudinaryImage, getFaceFromImage } = require('../../../libs/cloudinary.js');
+let { uploadCloudinaryImage, getFaceFromImage } = require('../../../../libs/cloudinary.js');
 
 let uploadProfileImage = async ({ query }, res) => {
   let { messenger_user_id, new_profile_image } = query;
@@ -8,15 +8,23 @@ let uploadProfileImage = async ({ query }, res) => {
   let person = await getPersonByMessengerID(messenger_user_id);
   let person_image_fields = Object.keys(person.fields).filter(field => field.startsWith('Profile Image URL')).map(field => person.fields[field]);
 
+  let new_profile_image_url = await uploadCloudinaryImage(
+    { image_url: new_profile_image }
+  );
+
+  let face_profile_image_url = getFaceFromImage(
+    { image_url: new_profile_image_url }
+  );
+
   let next_image_index = person_image_fields.length + 1;
   let update_data = {
-    ['Profile Image URL ' + next_image_index]: new_profile_image,
+    ['Profile Image URL ' + next_image_index]: face_profile_image_url,
   }
 
   let redirect_to_blocks = ['Profile Image Uploaded'];
 
   let updated_person = await updatePerson(update_data, person);
-  
+
   res.send({ redirect_to_blocks });
 }
 
