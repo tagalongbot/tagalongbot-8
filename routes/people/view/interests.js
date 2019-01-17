1e935d8c101492f2b415848ce8e71290d5caa8de
Glitch (the3dwin-tag-along) <none>
2019-01-17 22:14:53 +0000

    🌗🏐 Checkpoint ./routes/people/view/interests.js:701400/4039 ./routes/people/view/professions.js:701400/942

--- routes/people/view/interests.js
+++ routes/people/view/interests.js
@@ -5,18 +5,31 @@ let view_template = require('../../../views/check-boxes.marko');
 
 let { getPersonByMessengerID } = require('../../../libs/data/people.js');
 
-let viewProfileInterests = async ({ query }, res) => {
-  let { messenger_user_id } = query;
-
-  let person = await getPersonByMessengerID(messenger_user_id);
-  let person_interests = person.fields['Interests'];
-
+let getInterests = function () {
   let title = 'Interests';
-  let options = ['American TV Series', 'Anime/Manga', 'Books', 'Cycling', 'Dance', 'Fashion', 'Food', 'Gaming', 'Gym', 'Hollywood Movies', 'Internet', 'Karaoke', 'Korean Culture', 'Music', 'Painting/Doodling', 'Pets', 'Photography', 'Religion', 'Running', 'Science/Tech', 'Sports', 'Travel', 'Volunteering', 'Writing', 'Yoga/Meditation']
+  let check_boxes = ['American TV Series', 'Anime/Manga', 'Books', 'Cycling', 'Dance', 'Fashion', 'Food', 'Gaming', 'Gym', 'Hollywood Movies', 'Internet', 'Karaoke', 'Korean Culture', 'Music', 'Painting/Doodling', 'Pets', 'Photography', 'Religion', 'Running', 'Science/Tech', 'Sports', 'Travel', 'Volunteering', 'Writing', 'Yoga/Meditation']
     .map(option => {
       let checked = person_interests.includes(option);
       return { label: option, checked };
     });
+
+  let title = 'Professions';
+  let check_boxes = ['Accountant', 'Actor', 'Actuary', 'Architect', 'Artist', 'Aviator', 'Broker', 'Butcher', 'Chef', 'Consultant', 'Dental Hygienist', 'Dentist', 'Designer', 'Dietitian', 'Electrician', 'Engineer', 'Firefighter', 'Hairdresser', 'Health Professional', 'Journalist', 'Judge', 'Labourer', 'Lawyer', 'Librarian', 'Machinist', 'Mechanic', 'Medical Laboratory Scientist', 'Midwife', 'Musician', 'Operator', 'Paramedic', 'Pharmacist', 'Physician', 'Physiotherapist', 'Plumber', 'Police Officer', 'Psychologist', 'Radiographer', 'Scientist', 'Secretary', 'Software Developer', 'Statistician', 'Student', 'Surgeon', 'Surveyor', 'Teacher', 'Technician', 'Technologist', 'Tradesman', 'Veterinarian', 'Waiting staff']
+    .map(option => {
+      let checked = person_professions.includes(option);
+      return { label: option, checked };
+    });
+  
+  
+}
+
+let viewProfileInterestsProfessions = async ({ query }, res) => {
+  let { messenger_user_id } = query;
+
+  let person = await getPersonByMessengerID(messenger_user_id);
+  let person_interests = person.fields['Interests'];
+  let person_professions = person.fields['Professions'];
+
 
   let view_html = riot.render(
     check_boxes_tag,
@@ -29,4 +42,4 @@ let viewProfileInterests = async ({ query }, res) => {
   );
 }
 
-module.exports = viewProfileInterests;
+module.exports = viewProfileInterestsProfessions;
--- views/check-boxes.marko.js
+++ views/check-boxes.marko.js
@@ -23,13 +23,11 @@ function render(input, out, __component, component, state) {
   component_globals_tag({}, out);
 
   out.w(marko_str(input.view_html) +
-    "<script src=\"https://cdn.jsdelivr.net/npm/riot@3.11/riot.min.js\"></script><script type=\"text/javascript\" src=\"/tags/check-boxes.js\"></script><script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js\"></script><script>\n      // Messenger Extension\n      (function(d, s, id){\n        var js, fjs = d.getElementsByTagName(s)[0];\n        if (d.getElementById(id)) {return;}\n        js = d.createElement(s); js.id = id;\n        js.src = \"//connect.facebook.net/en_US/messenger.Extensions.js\";\n        fjs.parentNode.insertBefore(js, fjs);\n      }(document, 'script', 'Messenger'));\n    </script><script>\n      M.AutoInit();\n\n      let title = \"" +
-    marko_escapeScript(input.title) +
-    "\";\n      let messenger_user_id = \"" +
+    "<script src=\"https://cdn.jsdelivr.net/npm/riot@3.11/riot.min.js\"></script><script type=\"text/javascript\" src=\"/tags/check-boxes.js\"></script><script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js\"></script><script>\n      // Messenger Extension\n      (function(d, s, id){\n        var js, fjs = d.getElementsByTagName(s)[0];\n        if (d.getElementById(id)) {return;}\n        js = d.createElement(s); js.id = id;\n        js.src = \"//connect.facebook.net/en_US/messenger.Extensions.js\";\n        fjs.parentNode.insertBefore(js, fjs);\n      }(document, 'script', 'Messenger'));\n    </script><script>\n      M.AutoInit();\n\n      let messenger_user_id = \"" +
     marko_escapeScript(input.messenger_user_id) +
     "\";\n      let options = " +
     marko_escapeScript(input.options) +
-    ";\n\n      riot.mount(\n        '*',\n        { title, messenger_user_id, options }\n      );\n    </script>");
+    ";\n\n      riot.mount(\n        '*',\n        { messenger_user_id, options }\n      );\n    </script>");
 
   init_components_tag({}, out);
 
